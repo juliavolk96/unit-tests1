169bf3385e0c454f56a57b3d5eb453381a3609f3
Julia <juliavolk96@gmail.com>
2023-05-23 15:14:40 +0400

    update chunkFormat

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
+    chunkFormat: 'commonjs', 
   },
   module: {
     rules: [
